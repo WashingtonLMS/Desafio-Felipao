a713453f76f7a6bd98c9ce838b0d004e14aec8da
Washington Merlin <167993677+WashingtonLMS@users.noreply.github.com>
2024-11-02 18:11:57 -0300

    Adicionado xp
    Xp total na saida.

--- ClassificadorHeroi.js
+++ ClassificadorHeroi.js
@@ -1,40 +1,27 @@
 const hero = "Merlin"
-
-function randint(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-  
-let xp = randint(1, 13000);
-const classificacao = ('O herói ' + hero + ' está no nível de ') 
+function randint(max, min){
+    return Math.floor(Math.random() * (max - min + 1) +min)
+} 
+let xp = randint(0, 13000)
+let elo = 'o Herói '+ hero + ' Está no Elo'
 
 switch (true) {
     case xp <= 1000:
-    console.log(classificacao +'Ferro')
-    break
-    
+        console.log(elo + ' Bronze com a xp total de ' + xp)
+        break
     case xp <= 2000:
-    console.log(classificacao + 'Bronze')
-    break
-
+        console.log(elo + ' Prata com a xp total de ' + xp)
+        break
+    case xp <= 3000:
+        console.log(elo + ' Ouro com a xp total de ' + xp)
+        break
     case xp <= 5000:
-    console.log(classificacao + 'Prata')
-    break
-
+        console.log(elo + ' Ascendente com a xp total de ' + xp)
+        break
     case xp <= 8000:
-    console.log(classificacao + 'Ouro')
-    break
-
-    case xp <= 9000:
-    console.log(classificacao + 'Ascendente')
-    break
-
-    case xp <= 10000:
-    console.log(classificacao + 'Imortal')
-    break
-
+        console.log(elo + ' Imortal com a xp total de ' + xp)
+        break
     case xp >= 10001:
-    console.log(classificacao + 'Radiante') 
-} 
-
-
-
+        console.log(elo + ' Radiante com a xp total de ' + xp)
+        break
+}
